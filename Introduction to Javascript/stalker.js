565644f698abb29471cba761023e58bc2c36abb3
Steven Curry <recasas.curry@gmail.com>
2018-05-20 17:49:37 -0700

    made javascript name prompt file

---
+++ Introduction to Javascript/stalker.js
@@ -0,0 +1,5 @@
+let firstName = prompt("First Name:");
+let lastName = prompt("Last Name:");
+let age = prompt("Age:");
+console.log("Your full name is: " + firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.");
