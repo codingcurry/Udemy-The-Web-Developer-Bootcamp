e437d0189efebad7625a9f8a902d750bec7a94fa
Steven Curry <recasas.curry@gmail.com>
2018-05-21 08:34:09 -0700

    while loop exercises completed using for loops

--- Introduction to Javascript/whileLoops.js
+++ Introduction to Javascript/whileLoops.js
@@ -1,3 +1,4 @@
+console.log("WHILE LOOPS");
 let count;
 console.log("Print all numbers between -10 and 19");
 count = -10;
@@ -24,4 +25,23 @@ while (count<51) {
 		console.log(count);
 	}
 	count++;
+}
+console.log("FOR LOOPS");
+console.log("Print all numbers between -10 and 19");
+for (let i=-10; i<20; i++) {
+	console.log(i);
+}
+console.log("even numbers between 10 and 40");
+for (let i=10; i<41; i+=2) {
+	console.log(i);
+}
+console.log("odd numbers between 300 333");
+for (let i=301; i<334; i+=2) {
+	console.log(i);
+}
+console.log("numbers divisible by 5 and 3, between 5 and 50");
+for (let i=5; i<51; i++) {
+	if (i%5===0 && i%3===0) {
+		console.log(i);
+	}
 }
