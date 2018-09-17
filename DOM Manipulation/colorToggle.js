b82ec94bccd17726fb1b6386a411b7fa1f950248
Steven Curry <recasas.curry@gmail.com>
2018-09-16 21:49:09 -0700

    second color toggle method

--- DOM Manipulation/colorToggle.js
+++ DOM Manipulation/colorToggle.js
@@ -1,6 +1,13 @@
 var button = document.querySelector("button");
 
+//Method 1
+// button.addEventListener("click", function() {
+// 	if(document.body.style.background==="purple") document.body.style.background="white";
+//   	else document.body.style.background = "purple";
+// });
+
+
+//Method 2
 button.addEventListener("click", function() {
-	if(document.body.style.background==="purple") document.body.style.background="white";
-  	else document.body.style.background = "purple";
+	document.body.classList.toggle("purple");
 });
