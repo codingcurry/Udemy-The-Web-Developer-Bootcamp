47d083de72abd9047de272cf2a82fcfd5aa70dd3
Steven Curry <recasas.curry@gmail.com>
2018-09-19 14:12:07 -0700

    used new event listeners

---
+++ DOM Manipulation/todo.js
@@ -0,0 +1,15 @@
+let li = document.querySelectorAll("li");
+
+for(let i=0; i<li.length; i++) {
+	li[i].addEventListener("mouseover", function() {
+		this.classList.add("hover");
+	});
+
+	li[i].addEventListener("mouseout", function() {
+		this.classList.remove("hover");
+	});
+
+	li[i].addEventListener("click", function() {
+		this.classList.toggle("complete");
+	});
+}
