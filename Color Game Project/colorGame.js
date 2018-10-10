0e6e7abbb1654f486a0f82a20c3e39cfec31bf3a
Steven Curry <recasas.curry@gmail.com>
2018-10-10 10:31:58 -0700

    code refactoring on buttons

--- Color Game Project/colorGame.js
+++ Color Game Project/colorGame.js
@@ -6,48 +6,22 @@ let rgbDisplay = document.querySelector("#rgbCode");
 let message = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
-let easy = document.querySelector("#easy");
-let hard = document.querySelector("#hard");
+// let easy = document.querySelector("#easy");
+// let hard = document.querySelector("#hard");
+let modeButtons = document.querySelectorAll(".mode")
 
 resetButton.addEventListener("click", function() {
+	reset();
+});
+
+function reset() {
 	colors=generateRandomColors(mode);
 	pickedColor=pickColor();
 	rgbDisplay.textContent=pickedColor;
 	for(let i=0; i<squares.length; i++) {
-		squares[i].style.backgroundColor=colors[i];
-	}
-	h1.style.backgroundColor="steelblue";
-	message.textContent="";
-	this.textContent="New Colors";
-});
-
-hard.addEventListener("click", function() {
-	hard.classList.add("selected");
-	easy.classList.remove("selected");
-	mode=6;
-	colors = generateRandomColors(mode);
-	pickedColor = pickColor();
-	rgbDisplay.textContent=pickedColor;
-	for(let i=0; i<squares.length; i++) {
-		if(i>2) {
-			squares[i].style.display="block";
-		}
-		squares[i].style.backgroundColor=colors[i];
-	}
-	h1.style.backgroundColor="steelblue";
-	message.textContent="";
-});
-
-easy.addEventListener("click", function() {
-	easy.classList.add("selected");
-	hard.classList.remove("selected");
-	mode=3;
-	colors = generateRandomColors(mode);
-	pickedColor = pickColor();
-	rgbDisplay.textContent=pickedColor;
-	for(let i=0; i<squares.length; i++) {
 		if(colors[i]) {
 			squares[i].style.backgroundColor=colors[i];
+			squares[i].style.display="block";
 		}
 		else {
 			squares[i].style.display="none";
@@ -55,7 +29,21 @@ easy.addEventListener("click", function() {
 	}
 	h1.style.backgroundColor="steelblue";
 	message.textContent="";
-});
+	resetButton.textContent="New Colors";
+}
+
+for(let i=0; i<modeButtons.length; i++) {
+	modeButtons[i].addEventListener("click", function() {
+		//hard coded
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
+
+		this.classList.add("selected");
+		if(modeButtons[i].textContent==="Easy") mode=3;
+		else mode=6;
+		reset();
+	});
+}
 
 hard.classList.add("selected");
 rgbDisplay.textContent = pickedColor;
