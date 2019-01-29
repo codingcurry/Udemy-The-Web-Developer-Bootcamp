071cee56ca99912aac2beefa1c8a6888e060e9a8
Steven Curry <recasas.curry@gmail.com>
2019-01-29 12:33:13 -0800

    added input text functionality to todo list

--- Todo List/assets/js/todos.js
+++ Todo List/assets/js/todos.js
@@ -1,12 +1,20 @@
 //cross out items with a click
-$('li').click(function() {
+$('ul').on("click", "li", function() {
 	$(this).toggleClass("crossed-out");
 });
 
 //delete items by clicking the 'X'
-$('span').click(function(event) {
+$('ul').on("click", "span", function(event) {
 	$(this).parent().fadeOut(300, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
-})
+});
+
+//add new items in text input
+$('input[type="text"]').keypress(function(event) {
+	if(event.which===13) {
+		$('ul').append("<li><span>X</span> " + $(this).val() + "</li>");
+		$(this).val("");
+	}
+});
